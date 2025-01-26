e9947a17d69ba1ba23cee9e0a96a5de7740101a1
Astotts <55721055+Astotts@users.noreply.github.com>
2025-01-26 00:54:45 -0800

    Revert "God help us all"
    This reverts commit 56192fc6e577206a7259c10479485f50c2aeaf74, reversing
    changes made to 99ea7642efcefccc28a091ccbb34c3ea487000eb.

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using System.Collections;
-
 
 public class Movement : MonoBehaviour
 {
@@ -9,16 +7,7 @@ public class Movement : MonoBehaviour
     Rigidbody2D rb;
     [SerializeField] float jumpAmount;
     [SerializeField] float jumpDelay;
-    float elapsedTime = 1f;
-    private int currentSpriteIndex = 0;
-    [SerializeField] private float frameRate;
-    [SerializeField] private Sprite jumpSprite;
-
-    private bool running = false;
-
-    [SerializeField] private Sprite[] sprites;
-    [SerializeField] private SpriteRenderer spriteRenderer;
-    [SerializeField] private Sprite floatSprite;
+    float elapsedTime = 0f;
 
 
     bool isGrounded = false;
@@ -26,7 +15,6 @@ public class Movement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        spriteRenderer.sprite = sprites[0];
     }
 
     // Update is called once per frame
@@ -36,33 +24,11 @@ public class Movement : MonoBehaviour
         myVector.y = Input.GetAxisRaw("Vertical");
         if (isGrounded)
         {
-            if (myVector.x != 0)
-            {
-                if (!running)
-                {
-                    StartCoroutine("AnimateSprites");
-                }
-                if(myVector.x > 0) {
-                    spriteRenderer.flipX = false ;
-                }
-                else if (myVector.x < 0)
-                {
-                    spriteRenderer.flipX = true;   
-                }
-            }
-            //
-            else
-            {
-                running = false;
-                StopCoroutine("AnimateSprites");
-                spriteRenderer.sprite = sprites[0];  
-            }
-            //
             rb.linearVelocity = new Vector2(myVector.x * speed, rb.linearVelocity.y);
             if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
 
             {
-                spriteRenderer.sprite = jumpSprite;
+             
                 rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
                 isGrounded = false;
                 elapsedTime = jumpDelay;
@@ -71,10 +37,7 @@ public class Movement : MonoBehaviour
         }
         else if(Input.GetKey(KeyCode.Space) && elapsedTime <= 0f)
         {
-            spriteRenderer.sprite = floatSprite;
-            running = false;
-            StopCoroutine("AnimateSprites");
-       
+          
             rb.linearVelocity = new Vector2(myVector.x * speed, myVector.y * speed);
             rb.linearVelocity = Vector2.ClampMagnitude(rb.linearVelocity, speed);
         }
@@ -90,26 +53,6 @@ public class Movement : MonoBehaviour
             var velocity = rb.linearVelocity;
             isGrounded = true;
             rb.linearVelocity = new Vector2(velocity.x, 0);
-            spriteRenderer.sprite = sprites[0];
-        }
-    }
-
-    private IEnumerator AnimateSprites()
-    {
-        running = true;
-        while (running)
-        {
-            if (isGrounded)
-            {
-                spriteRenderer.sprite = sprites[currentSpriteIndex];
-                currentSpriteIndex = (currentSpriteIndex + 1) % sprites.Length;
-                yield return new WaitForSeconds(frameRate);
-            }
-            else
-            {
-                spriteRenderer.sprite = sprites[currentSpriteIndex];
-                yield return new WaitForSeconds(frameRate);
-            }
             
         }
     }
