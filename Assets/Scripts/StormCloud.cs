0379475ea59fde1779c27b3067538716036840a9
Samuel W <68044712+Samuel-KW@users.noreply.github.com>
2025-01-26 12:21:58 -0800

    feat: merged old best branch

---
+++ Assets/Scripts/StormCloud.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+public class StormCloud : MonoBehaviour
+{
+
+    public float minY = 500;
+    public GameObject targetObject;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (targetObject == null)
+        {
+            targetObject = GameObject.FindWithTag("Player");
+
+            if (targetObject == null )
+            {
+                Debug.LogError("Target Object is not defined and Player does not exist");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = targetObject.transform.position;
+        transform.position = new Vector3(pos.x, Math.Min(pos.y, minY), transform.position.z);
+    }
+}
